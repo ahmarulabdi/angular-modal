be1f3f400f75d93c4c0d0fae89634433b414eca2
ahmarulabdi <rndmjck@gmai.com>
2019-11-25 16:24:48 +0700

    adding ngb and bootstrap UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalContainerComponent } from './modal-container/modal-container.component';
+import { ModalContentComponent } from './modal-content/modal-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalContainerComponent,
+    ModalContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modal-content/modal-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-content',
+  templateUrl: './modal-content.component.html',
+  styleUrls: ['./modal-content.component.css']
+})
+export class ModalContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
